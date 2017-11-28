dc2c87c6b49ee32912cda133b81c6e427775c0f4
alanyair <32917118+alanyair@users.noreply.github.com>
2017-11-27 20:15:59 -0600

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,26 @@
+<footer class="Footer section_nav" id="contacto">
+	<div class="row container items-between">
+		<div class="col-xs12 col-sm7 m_bottom_20">
+			<h3 class="t-caption c-whtie">Dirección / Contacto</h3>
+			<div class="t-second m_bottom_20">
+				<p>Avenida San Claudio y 18 Sur, Colonia San Manuel Edificio 111A-109B, Ciudad Universitaria C.P. 72570 Puebla, México.</p>
+			</div>
+			<a href="#" class="t-link c-white m_bottom_20"><i class="icon-phone"></i> +52 (222) 2295500 ext. 7552</a> | <a href="#" class="t-link c-white"><i class="icon-email"></i> direccion<span class="arroba"></span>fcfm.buap.mx</a>
+		</div>
+		<div class="col-xs12 col-sm5">
+			<img src="./public/img/fcfm_footer.svg">
+		</div>
+	</div>
+
+
+
+	<div class="copy container clearfix">
+		<p>&copy; FCFM | 2016 </p>
+		<div  class="designer">
+			<a href="http://minz.design" class="designer__link" target="_blank">minz.design</a>
+		</div>
+	</div>
+</footer>
+
+
+
